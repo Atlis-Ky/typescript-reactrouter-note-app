1595abb63b1b6fb7f80f32368f74020968dff356
Ky <kylesmith97@outlook.com>
2025-03-27 12:31:37 +0000

    handled creation of new notes, local storage, and generating unique ID's

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+// Types
 
 export type Note = {
   id: string
@@ -11,7 +16,16 @@ export type Note = {
   title: string
   markdown: string
   tags: Tag[]
+}
 
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
 }
 
 export type Tag = {
@@ -19,13 +33,32 @@ export type Tag = {
   label: string
 }
 
+
+
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  //function to create a new note and store it in notes array above
+
+  function onCreateNote({tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}] // react library that creates a unique string id
+    })
+  }
+
   return (
 
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
