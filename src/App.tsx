fd0896692c77f0c2890fab22b4f2cf5da5850334
Ky <kylesmith97@outlook.com>
2025-03-28 13:58:57 +0000

    delete button logic setup and working

--- src/App.tsx
+++ src/App.tsx
@@ -81,6 +81,12 @@ function App() {
     setTags((prev) => [...prev, tag]);
   }
 
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
@@ -99,7 +105,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
